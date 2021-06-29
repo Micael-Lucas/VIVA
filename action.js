1ee3222e9e21c5b23b8fdb10206cffca30506cb6
Micael <micael.lucas.silva@gmail.com>
2021-06-28 22:24:41 -0300

    Correção do Footer

--- action.js
+++ action.js
@@ -4,3 +4,32 @@ var myInput = document.getElementById('myInput')
 myModal.addEventListener('shown.bs.modal', function () {
   myInput.focus()
 })
+
+$(function () {
+  var iframe = $('.main-content iframe')[0],
+      menu_links = $('.items li a'),
+      selected_link,
+      href;
+
+
+  $(window).on('hashchange', function() {
+
+      if(window.location.hash){
+          href = window.location.hash.substring(1);
+          selected_link = $('a[href$="'+href+'"]');
+
+          // Verifica se o hash é válido - deve existir como um dos itens do menu.
+          if(selected_link.length){
+              iframe.contentWindow.location.replace(href + '.html');
+
+              menu_links.removeClass('active');
+              selected_link.addClass('active');
+          }
+      }
+      else{
+          iframe.contentWindow.location.replace('Footer-with-logo.html');
+          menu_links.removeClass('active');
+          $(menu_links[0]).addClass('active');
+      }
+
+  });
